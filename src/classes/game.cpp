a45f82803d39f2f4576191c6ab55cacd15adc839
thales3006 <thales3006@gmail.com>
2025-01-12 15:19:08 -0300

    fix: gave up, best optimazation i can do

--- src/classes/game.cpp
+++ src/classes/game.cpp
@@ -40,9 +40,7 @@ void Game::run() {
         shaders[0].setMat4("view", player.camera.getMatrixView());
 
         chunkManager.renderChunks(shaders[0], player.chunkx, player.chunkz);
-
-        std::cout << player.velocity.x << " " << player.velocity.y << " " << player.velocity.z << " \n";
-        //std::cout << "FPS: "<< 1/deltaTime <<" \n";
+        std::cout << "FPS: "<< 1/deltaTime <<" \n";
         
 		glfwSwapBuffers(window);
 		glfwPollEvents();
