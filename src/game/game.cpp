2935c8cbd17ae3ea6952e9962c18860849150d50
Thales Janisch Santos <thales3006@gmail.com>
2026-04-02 21:54:33 -0300

    refactor: removed static methods from separated file

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -42,3 +42,22 @@ void Game::run() {
 
     glfwTerminate();
 }
+
+void Game::mouseMoveCallback(GLFWwindow *window, double xpos, double ypos) {
+    Game &game = *(static_cast<Game *>(glfwGetWindowUserPointer(window)));
+    static double xlast = xpos;
+    static double ylast = ypos;
+    game.inputManager.handleMouseMovementCallback(xpos - xlast, ylast - ypos);
+    xlast = xpos;
+    ylast = ypos;
+}
+
+void Game::mouseClickCallback(GLFWwindow *window, int button, int action, int mods) {
+    Game &game = *(static_cast<Game *>(glfwGetWindowUserPointer(window)));
+    game.inputManager.handleMouseClickCallback(window, button, action, mods);
+}
+
+void Game::keyboardCallback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    Game &game = *(static_cast<Game *>(glfwGetWindowUserPointer(window)));
+    game.inputManager.handleKeyboardCallback(window, key, scancode, action, mods);
+}
