6f12abf3a4b8a1f8aace2440a21a7b4b172b5c27
Thales Janisch Santos <thales3006@gmail.com>
2026-04-03 17:34:24 -0300

    refactor: removed M_PI constant from player

--- src/world/entity/player.cpp
+++ src/world/entity/player.cpp
@@ -1,9 +1,5 @@
 #include "world/entity/player.h"
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
 Player::Player(glm::vec3 position, glm::vec3 size)
     : Entity(position, size, glm::vec3(0.0f, 0.0f, 1.0f)), hotbarIndex(7) {}
 
